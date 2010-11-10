a9a1fc05b17af436fec03adaf007aeac563dd268
Janosch Machowinski <janosch.machowinski@dfki.de>
2010-11-10 17:36:14 +0100

    remove getLastSample
    The last sample is already replayed, and thus not available in the stream any more.

--- src/StreamAligner.hpp
+++ src/StreamAligner.hpp
@@ -47,15 +47,6 @@ namespace aggregator {
 
 	    bool getNextSample(item &sample) const
 	    {
-		if(buffer.size() < 2)
-		    return false;
-		
-		sample = *(buffer.begin()++);
-		return true;
-	    }
-
-	    bool getCurrentSample(item &sample) const
-	    {
 		if(buffer.empty())
 		    return false;
 		
@@ -258,17 +249,6 @@ namespace aggregator {
 	    assert( stream );
 
 	    stream->push( ts, data );
-	}
-
-	template <class T> bool getLastSample( int idx, std::pair<base::Time,T> &sample) const
-	{
-	    if( idx < 0 )
-		throw std::runtime_error("invalid stream index.");
-
-	    Stream<T>* stream = dynamic_cast<Stream<T>*>(streams[idx]);
-	    assert( stream );
-
-	    return stream->getCurrentSample(sample);
 	}
 
 	template <class T> bool getNextSample( int idx, std::pair<base::Time,T> &sample) const
